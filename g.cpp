34f81d9a7f4385f515ae983f7261f66c7620509e
root <root@localhost.localdomain>
2018-03-27 11:55:28 +0200

    gggggggggggggggggggg

--- g.cpp
+++ g.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <fstream>
 #include <streambuf>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 struct rule {
@@ -15,6 +17,8 @@ int main ()
     ifstream ifs ("test.txt", ifstream::in);
     string fileContents((istreambuf_iterator<char>(ifs)), istreambuf_iterator<char>());
 
+
+    // get rule count
     int ruleCount = 0;
 
     for (int i = 0; i < fileContents.length(); i++)
@@ -26,79 +30,52 @@ int main ()
     }
 
     rule rules[ruleCount];
+    vector<string> nonTerminals;
+
+    // fill rules
 
     int currentRule = 0;
-    int auxLeft = 0;
+    int aux = 0;
     for (int i = 0; i < fileContents.length(); i++)
     {
       if (fileContents.at(i) == '-')
       {
-        rules[currentRule].left = fileContents.substr(auxLeft, i-auxLeft);
-        auxLeft = i + 2;
+        string leftSide = fileContents.substr(aux, i - aux);
+        rules[currentRule].left = leftSide;
+        if (find(nonTerminals.begin(), nonTerminals.end(), leftSide) == nonTerminals.end()) // if vector does not contain
+        {
+          nonTerminals.push_back(leftSide);
+        }
+        aux = i + 2;
       }
       else if (fileContents.at(i) == '|')
       {
-        rules[currentRule].right = fileContents.substr(auxLeft, i-auxLeft);
+        rules[currentRule].right = fileContents.substr(aux, i - aux);
         rules[currentRule + 1].left = rules[currentRule].left;
         currentRule++;
-        auxLeft = i + 1;
+        aux = i + 1;
       }
       else if (fileContents.at(i) == '\n')
       {
-        rules[currentRule].right = fileContents.substr(auxLeft, i-auxLeft);
+        rules[currentRule].right = fileContents.substr(aux, i - aux);
         currentRule++;
-        auxLeft = i + 1;
+        aux = i + 1;
       }
     }
 
+    //for (auto i : nonTerminals) // access by value, the type of i is int
+    //    cout << i << endl;
 
-    // print rules
+    vector<string> currentStrings;
+    currentStrings.push_back(nonTerminals.front());
 
     for (int i = 0; i < ruleCount; i++)
     {
-      cout << rules[i].left << ", " << rules[i].right << endl;
+      if (rules[i].left.compare(axiom) == 0)
+      {
+        cout << rules[i].right << endl;
+      }
     }
 
-
-
-
-
-
-
-
-
-    //cout << asdf << endl << "wwwwwwwwwwwwwwwwwwwwww" << endl;
-    //char c = ifs.get();
-
-    //while (ifs.good())
-    //{
-    //  std::cout << c;
-    //  c = ifs.get();
-    //}
-
-    //ifs.close();
-/*
-    rule papa;
-    papa.left = "S";
-    papa.right = "dffasfsdsdfa";
-
-
-    cout << papa.right << endl;
-
-    size_t f = papa.right.find("fa");
-    papa.right.replace(f, std::string("fa").length(), "w");
-
-
-    cout << papa.right << endl;*/
-
-    /*string str = "asdf";
-    str.append("waawawa");
-
-    ofstream myfile;
-    myfile.open ("example.txt");
-    myfile << "Writing this to a file.\n";
-    myfile.close();
-
-    cout << str << '\n';*/
     return 0;
 }
